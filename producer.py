4ef67eae1af9189dbd34b398f472997cd7de5a88
tamdm-dsoft <tamdm@dsoft.com.vn>
2019-10-03 15:37:27 +0700

    add all

---
+++ producer.py
@@ -0,0 +1,26 @@
+import time
+import cv2
+from kafka import SimpleProducer, KafkaClient
+
+kafka = KafkaClient('localhost:9092')
+
+
+producer = SimpleProducer(kafka)
+
+topic = 'my-topic'
+
+def video_emitter(video):
+	video = cv2.VideoCapture(video)
+	print(' emitting.....')
+	while(video.isOpened):
+		success, image = video.read()
+		if not success:
+			break
+		ret, jpeg = cv2.imencode('.png', image)
+		producer.send_messages(topic, jpeg.tobytes())
+		time.sleep(0.2)
+	video.release()
+	print('done emitting')
+if __name__ == '__main__':
+	video_emitter('video.mp4')
+
